08c397253f724d26a355bdb7cceb8dd912c95b62
RiceGrainRain <mnmanasnavale@gmail.com>
2024-07-23 20:41:06 -0500

    updated menu and sounds

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,7 +1,6 @@
 extends Control
 
 
-@onready var cont = $MarginContainer/HBoxContainer/VBoxContainer/Continue
 @onready var new_game = $MarginContainer/HBoxContainer/VBoxContainer/NewGame
 @onready var options = $MarginContainer/HBoxContainer/VBoxContainer/Options
 @onready var exit = $MarginContainer/HBoxContainer/VBoxContainer/Exit
@@ -9,8 +8,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Signal connections.
-	cont.button_down.connect(on_cont_down)
+
+
 	new_game.button_down.connect(on_ng_down)
 	options.button_down.connect(on_options_down)
 	exit.button_down.connect(on_exit_down)
@@ -18,13 +17,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var current_scene = get_tree().current_scene
+	if current_scene:
+		if $AudioStreamPlayer.playing == false:
+			$AudioStreamPlayer.play()
+	else:
+		$AudioStreamPlayer.stop()
 	pass
 
 
-func on_cont_down() -> void:
-	pass
-	
-	
 func on_ng_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_01/level_01.tscn")
 	TransitionScreen.transition()
