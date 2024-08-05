0fdb2013600da84cf0f197c1deb2b1995b49d84f
weyum11234 <wh2484@nyu.edu>
2024-08-05 15:14:57 -0400

    finish level02 cleanup

---
+++ entities/enemy.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var body = $StaticBody2D
+@onready var sprite = $StaticBody2D/AnimatedSprite2D
+@onready var start = $Start
+@onready var end = $End
+@onready var target = $End
+@export var speed = 100
+
+func _ready():
+	sprite.play()
+
+func _process(delta):
+	# Switch targets when reaching end points.
+	if body.position == start.position:
+		target = end
+		body.scale.x *= -1
+	elif body.position == end.position:
+		target = start
+		body.scale.x *= -1
+	# Move.
+	body.position = body.position.move_toward(target.position, speed * delta)
--- obstacles/trampoline.gd
+++ obstacles/trampoline.gd
@@ -1,10 +1,10 @@
 extends AnimatableBody2D
 
-@export var tramp_jump = 0
 @export var tramp_jump_x = 0
+@export var tramp_jump_y = -1000
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Player":
-		body.velocity.y = tramp_jump
+	if body.is_in_group("player"):
 		body.velocity.x = tramp_jump_x
+		body.velocity.y = tramp_jump_y
 		
