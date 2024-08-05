00fd4bdbbeca2e4455fe6b256859bb1c4e03f260
weyum11234 <wh2484@nyu.edu>
2024-08-05 12:13:27 -0400

    cloud cleanup

--- decorations/clouds.gd
+++ decorations/clouds.gd
@@ -1,44 +1,17 @@
-extends CharacterBody2D
+# Used for cloud_a, cloud_b, and cloud_c
+extends Node2D
 
-# Define the movement distance and speed
-@export var movement_distance: float = 10.0
-@export var movement_speed: float = 10.0
-
-# The original position of the node
-var original_position: Vector2
-var target_position: Vector2
-@export var direction: bool = true # true for forward, false for backward
-var moving_forward: bool = true
-
-func _ready():
-	$AnimatedSprite2D.play()
-	original_position = position
-	if direction:
-		target_position = original_position + Vector2(movement_distance, 0)
-	else:
-		target_position = original_position - Vector2(movement_distance, 0)
+@onready var sprite = $AnimatedSprite2D
+@onready var start = $Start
+@onready var end = $End
+@onready var target = $End
+@export var speed = 100
 
 func _process(delta):
-	var movement_amount = movement_speed * delta
-	if direction:
-		if moving_forward:
-			position.x += movement_amount
-			if position.x >= target_position.x:
-				position.x = target_position.x
-				moving_forward = false
-		else:
-			position.x -= movement_amount
-			if position.x <= original_position.x:
-				position.x = original_position.x
-				moving_forward = true
-	else:
-		if moving_forward:
-			position.x -= movement_amount
-			if position.x <= target_position.x:
-				position.x = target_position.x
-				moving_forward = false
-		else:
-			position.x += movement_amount
-			if position.x >= original_position.x:
-				position.x = original_position.x
-				moving_forward = true
+	# Switch targets when reaching end points.
+	if sprite.position == start.position:
+		target = end
+	elif sprite.position == end.position:
+		target = start
+	# Move.
+	sprite.position = sprite.position.move_toward(target.position, speed * delta)
