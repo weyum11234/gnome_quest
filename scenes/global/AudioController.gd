ff3a06c51d8424800d518e6f8fa7f1bd769768bd
weyum11234 <wh2484@nyu.edu>
2024-08-09 22:16:50 -0400

    Revert "Merge branch 'main' of https://github.com/weyum11234/gnome_quest"
    This reverts commit b8840e57bb0163b28fbab31b41ab1c97c27e2bcf, reversing
    changes made to 0ce9800a6f17651f26e03327d0980baedc197e73.

--- scenes/global/AudioController.gd
+++ scenes/global/AudioController.gd
@@ -1,6 +1,8 @@
 extends Node
 
 # Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 func _process(delta):
 	var current_scene = get_tree().current_scene
