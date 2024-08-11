75b14e8b8784ffa36e9f924b33dfa381bb3e83b4
RiceGrainRain <mnmanasnavale@gmail.com>
2024-08-11 18:23:33 -0500

    working pub-lobby!
    yippe!

--- scenes/global/AudioController.gd
+++ scenes/global/AudioController.gd
@@ -5,8 +5,10 @@ extends Node
 func _process(delta):
 	var current_scene = get_tree().current_scene
 	if current_scene != null:
-		if current_scene.name == "MainMenu" or current_scene.name == "Settings":
-			if $AudioStreamPlayer.playing == false:
+		print(current_scene.name)
+		var play_scenes = ["MainMenu", "Settings", "join_lobby"]
+		if current_scene.name in play_scenes:
+			if not $AudioStreamPlayer.playing:
 				$AudioStreamPlayer.play()
 		else:
 			$AudioStreamPlayer.stop()
--- scenes/select/steam_lobby.gd
+++ scenes/select/steam_lobby.gd
@@ -6,16 +6,27 @@ var peer = SteamMultiplayerPeer.new()
 @onready var ms = $MultiplayerSpawner
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	ms.spawn_function = spawn_level
 	peer.lobby_created.connect(_on_lobby_created)
 	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	$LobbyContainer.hide()
+	$VBoxContainer3/Back.hide()
 	open_lobby_list()
+	
 
 func spawn_level(data):
 	var a = (load(data) as PackedScene).instantiate()
 	return a
+	
+func _on_public_pressed():
+	$VBoxContainer/Label2.hide()
+	$VBoxContainer/private.hide()
+	$VBoxContainer/public.hide()
+	$VBoxContainer/host.hide()
+	$VBoxContainer2/Exit.hide()
+	$LobbyContainer.show()
+	$VBoxContainer3/Back.show()
 	
 func _on_host_pressed():
 	peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC)
@@ -51,3 +62,21 @@ func _on_lobby_match_list(lobbies):
 		but.set_size(Vector2(100, 5))
 		but.connect("pressed", Callable(self, "join_lobby").bind(lobby))
 		$LobbyContainer/Lobbies.add_child(but)
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
+
+
+func _on_back_pressed():
+	$LobbyContainer.hide()
+	$VBoxContainer3/Back.hide()
+	$VBoxContainer/Label2.show()
+	$VBoxContainer/private.show()
+	$VBoxContainer/public.show()
+	$VBoxContainer/host.show()
+	$VBoxContainer2/Exit.show()
+	
+
