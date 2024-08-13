24574915d9c99ed725c67cfdf57e5efb4bfcdc8e
weyum11234 <wh2484@nyu.edu>
2024-08-13 09:53:44 -0500

    removed prints

--- scenes/level_04/MultiplayerSpawner.gd
+++ scenes/level_04/MultiplayerSpawner.gd
@@ -4,19 +4,15 @@ extends MultiplayerSpawner
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("yeety")
 	spawn_function = spawnPlayer
 	if is_multiplayer_authority():
-		print("???")
 		spawn(1)
-		print("HUHUHUHUH")
 		multiplayer.peer_connected.connect(spawn)
 		multiplayer.peer_disconnected.connect(removePlayer)
 var players = {}
 
 # Spawns a player instance and adds it to the scene tree with replication.
 func spawnPlayer(data):
-	print("beety")
 	var p = PlayerScene.instantiate()
 	p.set_multiplayer_authority(data)
 	players[data] = p
