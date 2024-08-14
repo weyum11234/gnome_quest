b4c7c631aa78ffda8c2e0ffa693162c8a6f92543
weyum11234 <wh2484@nyu.edu>
2024-08-14 16:32:42 -0500

    bird syncing

--- scenes/level_04/MultiplayerSpawner.gd
+++ scenes/level_04/MultiplayerSpawner.gd
@@ -2,21 +2,21 @@ extends MultiplayerSpawner
 
 @onready var PlayerScene = preload("res://entities/players/player.tscn")
 
+var players = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_function = spawnPlayer
 	if is_multiplayer_authority():
 		spawn(1)
-		multiplayer.peer_connected.connect(spawn)
+		multiplayer.peer_connected.connect(spawnPlayer)
 		multiplayer.peer_disconnected.connect(removePlayer)
-var players = {}
 
 # Spawns a player instance and adds it to the scene tree with replication.
 func spawnPlayer(data):
 	var p = PlayerScene.instantiate()
 	p.set_multiplayer_authority(data)
 	players[data] = p
-	#add_child(p, true)  # Ensure the node is added with replication
 	return p
 
 # Removes a player instance from the scene tree.
