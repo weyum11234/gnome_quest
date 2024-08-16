204ba9eff45c0a2b44323ec62f250c9bd36ed480
weyum11234 <wh2484@nyu.edu>
2024-08-16 17:26:42 -0500

    removed server check when starting game

--- scenes/startup/startup.gd
+++ scenes/startup/startup.gd
@@ -70,8 +70,8 @@ func join_lobby(l_id : int):
 
 func start_game():
 	$UI.hide()
-	if multiplayer.is_server():
-		change_level.call_deferred(first_scene)
+	#if multiplayer.is_server():
+	change_level.call_deferred(first_scene)
 		
 func change_level(scene : PackedScene):
 	var level = $Level
