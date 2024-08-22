826129fb1ca9aee94a4c0ed5e2d1957fd259309b
weyum11234 <wh2484@nyu.edu>
2024-08-22 15:36:23 -0500

    teleport fix

--- entities/teleporter/teleporter.gd
+++ entities/teleporter/teleporter.gd
@@ -1,18 +1,15 @@
 extends Node2D
 
-@onready var ray = $RayCast2D
 var player : Object
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player:
-		if not ray.is_colliding():
-			player.global_position = get_global_mouse_position()
-			reset()
+		player.global_position = get_global_mouse_position()
+		reset()
 
 func use(player : Object):
 	self.player = player
-	ray.target_position = get_local_mouse_position()
 
 func alt_use(player : Object):
 	pass
