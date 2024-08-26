0264c3abfaf03e7ca79fa58fea633101f997c8ed
weyum11234 <wh2484@nyu.edu>
2024-08-26 16:48:43 -0500

    checkpoint stuff

--- obstacles/checkpoint.gd
+++ obstacles/checkpoint.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-@onready var animation = $AnimationPlayer
 @onready var audio = $AudioStreamPlayer2D
 var players : Array
 
-
+func _ready():
+	$AnimatedSprite2D.play("default")
 
 func _on_body_entered(body):
 	if body not in players:
 		body.spawn_position = self.global_position
-		animation.play("raise")
+		$AnimatedSprite2D.play("raise")
 		audio.stream = load("res://assets/sounds/mixkit-retro-game-notification-212.wav")
 		audio.play()
 		players.append(body)
