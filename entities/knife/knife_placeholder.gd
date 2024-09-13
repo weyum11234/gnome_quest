410713458af37dc6c67b829cce5c993ff46be961
weyum11234 <wh2484@nyu.edu>
2024-09-12 22:37:15 -0400

    mouse click test

--- entities/knife/knife_placeholder.gd
+++ entities/knife/knife_placeholder.gd
@@ -4,6 +4,7 @@ var id : int
 var parent : Node
 
 func use(player : Object):
+	print("used")
 	var real_knife = load("res://entities/knife/knife.tscn").instantiate()
 	real_knife.set_values(id, player)
 	parent.add_child(real_knife)
